3d1f487ef2974e567337c717f48ed79a017f646e
galeheidi <kgaletax@aol.com>
2018-06-20 19:35:10 +0000

    automatically backed up by learn

--- summer_olympics.rb
+++ summer_olympics.rb
@@ -5,13 +5,12 @@ summer_olympics={
 "Beijing" => "2008",
 "London" => "2012", 
 }
-summer_olympics= ["sydney","athens","Beijing""London"]
 summer_olympics.insert(0,"atlanta")
-puts "the 2004 summer olympics took place in #{"
+puts summer_olympics
 
 summer_olympics["Atlanta"]= "1996"
 puts summer_olympics 
 puts summer_olympics.class
-puts "The #{year} summer olympics took placein#{summer_olympics}"
+puts "the #{year} summer olympics took place in #{summer_olympics}"
 end 
 summer_olympics
