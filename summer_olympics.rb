eda8ea33a582b9b0e0bc571fdb464853b191ac14
galeheidi <kgaletax@aol.com>
2018-06-20 19:28:21 +0000

    automatically backed up by learn

--- summer_olympics.rb
+++ summer_olympics.rb
@@ -4,12 +4,11 @@ summer_olympics={
 "Athens" => "2004",
 "Beijing" => "2008",
 "London" => "2012", 
+end
 }
 def year ("2000", "2004","2008","2012","1996")
 summer_olympics["Atlanta"]= "1996"
 puts summer_olympics 
 puts summer_olympics.class
 puts "The #{year} summer olympics took placein#{summer_olympics}"
-end
 end 
-end
